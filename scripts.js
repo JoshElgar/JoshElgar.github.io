ef73aedee82362eeb7796141be0b4108db2f7dce
JoshElgar <joshelgar@gmail.com>
2016-04-17 19:18:21 +0100

    added bottom bar, about / faq / modified positions of head area / mainsection

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 function changeShortenedLink() {
     document.getElementById("shortenedLink").value = "www.goo.gl";
 }
+
+function navigateToLink() {
+    var x = document.getElementById("shortenedLink").value;
+    var y = "http://"
+    var url = x.concat(y);
+    window.location.href = "url";
+}
